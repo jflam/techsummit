8b187faae402b16454fa7426b3ab7a30419b7160
John Lam <jlam@iunknown.com>
2018-10-21 11:34:49 -0700

    Removing old files from root directory (should have git moved by whatever)

--- get_data.py
+++
@@ -1,9 +0,0 @@
-from sklearn import datasets
-
-def get_data():
-    
-    digits = datasets.load_digits()
-    X_digits = digits.data
-    y_digits = digits.target
-
-    return { "X" : X_digits, "y" : y_digits }
